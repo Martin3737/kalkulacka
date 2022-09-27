86751405fc8a3cda8ceee5cd9b632c6fcaadab60
Kučera Martin <Kucera.Ma.2022@skola.ssps.cz>
2022-09-27 09:40:55 +0200

    init

---
+++ kalkulačka1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalkulačka1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, vysledek;
+            string operace;
+
+            num1 = double.Parse(Console.ReadLine());
+            num2 = double.Parse(Console.ReadLine());
+            operace = Console.ReadLine();
+            vysledek = 0;
+            if (operace == "+")
+            {
+                vysledek = num1 + num2;
+            }
+            else if (operace == "-")
+            {
+                vysledek = num1 - num2;
+            }
+            else if (operace == "*")
+            {
+                vysledek = num1 * num2;
+            }
+            else if (operace == "/")
+            {
+                vysledek = num1 / num2;
+            }
+
+            Console.WriteLine(vysledek);
+
+
+        }   
+
+    }
+}
